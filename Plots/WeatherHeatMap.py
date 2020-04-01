da7d538cc0b19b008463ab7e03f8a25fa782ffe4
jcmecham <jmecham@uncc.edu>
2020-04-01 13:02:43 -0400

    group submission commit(update)

--- Plots/WeatherHeatMap.py
+++ Plots/WeatherHeatMap.py
@@ -9,19 +9,18 @@ df = pd.read_csv('../Datasets/Weather2014-15.csv')
 data = [go.Heatmap(x=df['day'],
                    y=df['month'],
                    z=df['record_max_temp'],
-                   colorscale='Jet')]
+                   colorscale='Jet'
+                   )
+                   ]
 
 # Preparing layout
-layout = go.Layout(title='Average temp per week per month', xaxis_title="Day of Week",
+layout = go.Layout(title='Record Max temp per day and month', xaxis_title="Day of Week",
                    yaxis_title="Month of year")
 
 # Plot the figure and saving in a html file
 fig = go.Figure(data=data, layout=layout)
 
-# labels=dict(x="Week of the month", y="Time of Day", color="Productivity"),
-#                 x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday','Sunday'],
-#                 y=['January', 'February', "March", "April", "May", "June", "July", "August", "September", "October",
-#                    "November", "December"])
+
 
 
 pyo.plot(fig, filename='WeatherHeatmap.html')
